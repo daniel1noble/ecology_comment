0878a0279dca7ff27e85bd54436dcecadf5ff28d
Daniel Noble <daniel.wa.noble@gmail.com>
2020-10-11 11:28:47 +1100

    Changed all figures for ErrorRate so that Experiment 2 plotted correctly now as well. Just adding in the independence to keep figure consistent. Cross checked with paper. Note that sim data is missing because of a problem runing the last few scenerios with cor = 0.9 and tau = 1, and 0.5. This needs fixing. Not sure what happened, but I think the computer must have run out of memory. In anycase, we need to try some more sophisticated df corrections and drop out some sceneriods to make plots a little more digestable.

--- DataS1/Figure_ErrorRate.R
+++ DataS1/Figure_ErrorRate.R
@@ -1,11 +1,12 @@
+pdf(width = 8.572687, height =  9.118943, file = "Figure3_NewSims.pdf")
 #Define the layout of multiple panel figure using layout matrix#
 layout.mat = rbind(c(1:2),c(1:2),c(1:2),c(11:12),c(3:4),c(3:4),c(3:4),c(5:6),c(5:6),c(5:6),c(13:14),c(7:8),c(7:8),c(7:8),c(9:10),c(9:10),c(9:10))
 
-method.col = c(rgb(0,0,0),rgb(0.9,0.6,0),rgb(0.8,0.4,0),rgb(0,0.45,0.7),rgb(0.35,0.7,0.9), rgb(0.35,0.6,0.8), rgb(0.35,0.5,0.5), rgb(0.40,0.5,0.8), rgb(0.40,0.2,0.3), rgb(0.23,0.1,0.1))
+method.col = c(rgb(0,0,0),rgb(0.9,0.6,0),rgb(0.8,0.4,0),rgb(0,0.45,0.7),rgb(0.35,0.7,0.9), rgb(0.35,0.6,0.8), rgb(0.35,0.5,0.5), rgb(0.40,0.5,0.8), rgb(0.40,0.2,0.3), rgb(0.8,0.1,0.1))
 
 #Set up plotting device and figure margins#
 
-quartz(w=6.5, h=9)
+#quartz(w=8.572687, h=9.118943)
 layout(mat=layout.mat)
 par(mar=c(0,0,0,0), oma=c(4,4,3,2))
 
@@ -14,7 +15,7 @@ par(mar=c(0,0,0,0), oma=c(4,4,3,2))
 #######################
 ##Independent studies##
 #######################
-	load("IIDStudy.RData")
+	load("./DataS1/IIDStudy.RData")
 
 	#Calculate error rate, and the confidence interval of error rate#
 	error = array(dim=c(scenario, 5))
@@ -70,12 +71,6 @@ load("EqualStudy.RData")
 #Calculate error rate and the confidence interval of error rate#
 error = array(dim=c(scenario, 10))
 error.ci = array(dim=c(scenario, 10))
-#NOTE THAT for scenario 6, there was a very tiny mix up in code and CI's were flipped between param.low (which is the upper CI) and param.up (which is actually the lower CI). Fix this first
-parm.low6 <- parm.up[,,6]
-parm.high6 <- parm.low[,,6]
-
-parm.low[,,6] <- parm.low6
-parm.up[,,6] <- parm.high6
 
 cover = (parm.low<0) + (parm.up>0)
 
@@ -136,7 +131,7 @@ for(i in c(1,3,7,9)){
 	if(i %in% c(7,8,9)){mtext(expression(rho==0.9), side=1, adj=0.5, cex=0.8, line=-1.3)}
 	#mtext(paste0("(",LETTERS[3+i],")"), side=1, adj=0.02, line=-1.35, cex=0.75)
 }
-quartz.save(type = "pdf", bg = "white")
+#quartz.save(type = "pdf", bg = "white")
 
 
 ##################################
@@ -146,8 +141,8 @@ quartz.save(type = "pdf", bg = "white")
 #Load results from simulations that allow tau to vary#
 #Because different simulation files using the same variable name, save results in new variable os that it can be plotted together with the unequal study with constant tau across paper scenario#
 load("UnequalStudy_VaryTau.RData")
-error.varytau = array(dim=c(scenario,5))
-error.ci.varytau = array(dim=c(scenario,5))
+error.varytau = array(dim=c(scenario,10))
+error.ci.varytau = array(dim=c(scenario,10))
 cover.varytau = (parm.low<0) + (parm.up>0)
 
 for(i in 1:scenario){
@@ -158,8 +153,8 @@ for(i in 1:scenario){
 load("UnequalStudy.RData")
 
 #Calculate error rate and the confidence interval of error rate#
-error = array(dim=c(scenario, 5))
-error.ci = array(dim=c(scenario, 5))
+error = array(dim=c(scenario, 10))
+error.ci = array(dim=c(scenario, 10))
 cover = (parm.low<0) + (parm.up>0)
 for(i in 1:scenario){
 	error[i,] = 1-colSums(cover[i,,]==2,na.rm=T)/iteration
@@ -183,8 +178,8 @@ for(i in c(1,3,4,6)){
 		rate = error[size[i,1],1]
 		error[size[i,1],1] = 999
 	}
-	plot(error[size[i,1],]~c(1:5), xlim=c(0.5,23.5), ylim=c(0,0.13), pch=19, axes=F, col=method.col)
-	arrows(x0=c(1:5),y0=error[size[i,1],]-error.ci[size[i,1],],y1=error[size[i,1],]+error.ci[size[i,1],], length=0, col=method.col)
+	plot(error[size[i,1],]~c(1:10), xlim=c(0.5,47.5), ylim=c(0,0.13), pch=19, axes=F, col=method.col)
+	arrows(x0=c(1:10),y0=error[size[i,1],]-error.ci[size[i,1],],y1=error[size[i,1],]+error.ci[size[i,1],], length=0, col=method.col)
 	box()
 	abline(h=0.05, lty=2, col="grey")
 	#Directly note value of error rate if too high to show on figure#
@@ -198,25 +193,25 @@ for(i in c(1,3,4,6)){
 		rate = error[size[i,j],1]
 		error[size[i,j],1] = 0.5
 		}
-		points(error[size[i,j],]~c(((j-1)*6+1):((j-1)*6+5)), pch=19, col=method.col)
-		arrows(x0=c(((j-1)*6+1):((j-1)*6+5)), y0=error[size[i,j],]-error.ci[size[i,j],], y1=error[size[i,j],]+error.ci[size[i,j],], length=0, col=method.col)
-		abline(v=6*(j-1), lty=2, col="grey")
+		points(error[size[i,j],]~c(((j-1)*11+1):((j-1)*11+10)), pch=19, col=method.col)
+		arrows(x0=c(((j-1)*11+1):((j-1)*11+10)), y0=error[size[i,j],]-error.ci[size[i,j],], y1=error[size[i,j],]+error.ci[size[i,j],], length=0, col=method.col)
+		abline(v=11*(j-1), lty=2, col="grey")
 		if(error[size[i,j],1]>0.1){
-			points(x=(j-1)*6+1, y=0.11, pch=19)
-			text(x=(j-1)*6+1, y=0.1, labels=paste0(round(100*rate,0),"%"), cex=0.85)
+			points(x=(j-1)*11+1, y=0.11, pch=19)
+			text(x=(j-1)*11+1, y=0.1, labels=paste0(round(100*rate,0),"%"), cex=0.85)
 			}
 	}
 	if(error.varytau[i,1]>0.1){
 		rate = error.varytau[i,1]
 		error.varytau[i,1] = 0.5
 	}
-	points(error.varytau[i,]~c(19:23), pch=19, col=method.col)
-	arrows(x0=c(19:23), y0=error.varytau[i,]-error.ci.varytau[i,], y1=error.varytau[i,]+error.ci.varytau[i,], length=0, col=method.col)
+	points(error.varytau[i,]~c(36:45), pch=19, col=method.col)
+	arrows(x0=c(36:45), y0=error.varytau[i,]-error.ci.varytau[i,], y1=error.varytau[i,]+error.ci.varytau[i,], length=0, col=method.col)
 	if(error.varytau[i,1]>0.1){
-		points(x=19, y=0.11, pch=19)
-		text(x=19, y=0.1, labels=paste0(round(100*rate,0),"%"), cex=0.85)
+		points(x=36, y=0.11, pch=19)
+		text(x=36, y=0.1, labels=paste0(round(100*rate,0),"%"), cex=0.85)
 		}
-	abline(v=18, lty=2, col="grey")
+	abline(v=34, lty=2, col="grey")
 	mtext(paste0("Mean:",paper.mean[i]),side=1,adj=0.95,line=-2.3, cex=0.75)
 	mtext(paste0("SD:",paper.sd[i]),side=1, adj=0.95,line=-1.3, cex=0.75)
 	mtext(expression(tau==0.1), side=3, adj=0.06, cex=0.8, line=-1.4)
@@ -237,5 +232,6 @@ mtext("Experiment 2: unequal correlation", outer=T, line=-39, font=2, adj=1)
 mtext("Error rate", side=2, outer=T, line=2.5, font=2)
 
 #Legends#
-mtext("Methods:", outer=T, line=0.5, font=2, adj=0, cex=0.95)
+mtext("Methods:", outer=T, line=0.5, font=2, adj=0, cex=0.70)
 legend("topleft", legend=c("1","2","3","4","5", "6", "7", "8", "9", "10"), pch=19, xpd=NA, inset=c(-0.75,-4.88), bty="n", horiz=T, cex=1.35, col=method.col)
+dev.off()
